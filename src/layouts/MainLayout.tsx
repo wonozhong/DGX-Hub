025d781f9f254fa3a88e1471be21704e28174cc7
wonozhong <44634889+wonozhong@users.noreply.github.com>
2026-01-11 03:00:25 +0700

    Fix broken DGX logo by using local SVG asset

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -82,7 +82,7 @@ export default function MainLayout() {
                   {/* Sidebar component for mobile */}
                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
                     <div className="flex h-16 shrink-0 items-center">
-                      <DGXLogo className="h-8 text-black" />
+                      <img src="https://dragonixinteractive.com/wp-content/uploads/2025/01/DGX-Logo-White-Transparent-300x78.png" alt="DGX Logo" className="h-8 filter invert" />
                     </div>
                     <nav className="flex flex-1 flex-col">
                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -148,7 +148,7 @@ export default function MainLayout() {
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
-               <DGXLogo className="h-8 text-black" />
+               <img src="https://dragonixinteractive.com/wp-content/uploads/2025/01/DGX-Logo-White-Transparent-300x78.png" alt="DGX Logo" className="h-8 filter invert" />
             </div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
