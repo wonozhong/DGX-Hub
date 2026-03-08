3f32722149dc480f85e10671cb3c55343d42cfda
wonozhong <44634889+wonozhong@users.noreply.github.com>
2026-03-09 05:21:43 +0700

    chore: Switch to Self-Hosted Supabase and cleanup migration scripts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gmhzojyxtkxaewkhtlmj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtaHpvanl4dGt4YWV3a2h0bG1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNDI4MTQsImV4cCI6MjA4MzYxODgxNH0.IDbElt7OQ3dGGbsth12IpVvJEHVSgw4s6lXRyO3U6Zs';
+const supabaseUrl = 'https://api.dragonixinteractive.com';
+const supabaseAnonKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3Mjg5MzYyMCwiZXhwIjo0OTI4NTY3MjIwLCJyb2xlIjoiYW5vbiJ9.3qPt7Rx9Lfv7rrL8T5Pl6jqcSO9uLbK0o5sqb7_p2L4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
